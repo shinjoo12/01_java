4a33fadb5c4c72b1de052ff5958274d65679ff10
jjoo <zhuyaan93@gmail.com>
2024-04-11 18:31:38 +0900

    first commit

---
+++ chap01-literal-and-variable/src/main/java/com/ohgiraffers/section01/literal/Application01.java
@@ -0,0 +1,28 @@
+package com.ohgiraffers.section01.literal;
+
+public class Application01 {
+    public static void main(String[] args) {
+        //리터럴이란 값 자체를 의미한다.
+
+        // 정수형 리터럴
+        System.out.println(123);
+        //시스템 패키지안에 참조(.)해서 out으로 들어가서 메써드를 이욯애서 값을 출력
+
+        //실수형 리터럴
+        System.out.println(3.14F);
+        //문자형 리터럴
+        System.out.println('A');
+        //문자열형 리터럴
+        System.out.println("hello world");
+
+        //"12345" 문자열
+        System.out.println("12345");
+        System.out.println("123.45");
+
+        // 논리형 리터럴
+        System.out.println(true);
+        System.out.println(false);
+
+    }
+
+}
---
+++ chap01-literal-and-variable/src/main/java/com/ohgiraffers/section01/literal/Application02.java
@@ -0,0 +1,36 @@
+package com.ohgiraffers.section01.literal;
+
+public class Application02 {
+    public static void main(String[] args) {
+      // 2+2 2-2 2/2 2*2
+        //화면 출력
+        System.out.println(2+2);
+        System.out.println(2-2);
+        System.out.println(2/2);
+        System.out.println(2*2);
+        //123 % 10
+        System.out.println(123%10);
+
+        //1.23 + 1.23
+        //1.23 - 1.23
+        //1.23 * 1.23
+        //1.23 / 1.23
+        //1.23 % 1.0
+        System.out.println(1.23 + 1.23);
+        System.out.println(1.23 - 1.23);
+        System.out.println(1.23 * 1.23);
+        System.out.println(1.23 / 1.23);
+        System.out.println(1.23 % 1.0);
+
+        // "123" + "12"  문자열+문자열
+        System.out.println("123"+"12");
+        //"123" - "12"    문자열 뺄수없음
+        //"123" * "12"
+        //"123"+12
+        System.out.println("123"+12);    //문자열+숫자 12312
+
+
+
+
+    }
+}
